eeb1330ed63a81c1a00cdc285e474fb35d40552a
jkky-98 <aal2525@ajou.ac.kr>
2023-10-28 11:16:53 +0900

    update component day-score

--- src/components/Day.vue
+++ src/components/Day.vue
@@ -1,16 +1,83 @@
 <template>
-  <div class="hello">
-    날짜
+  <div class="day">
+    <h1 class="day-title">
+      {{$route.params.date}} 
+      <!--라우터에 동적 파라미터인 :date를 받아옴-->
+    <ul class='day-list'>
+      <li
+        v-for="item in items"
+        :key="item.id"
+        class="day-list-item"
+  
+      >
+        <div class='time'> <!--시간부분-->
+          <h2>{{item.id}}
+          </h2>
+        </div>
+
+        <div class='action'> <!--행동부분-->
+          <input type="text" v-model="item.action"
+          placeholder="한 일을 입력하세요">
+        </div>
+
+        <day-score
+          @onUpdateScore="onUpdateScore"
+          :item="item">
+          
+        </day-score>
+
+        <div class='note'>
+          <textarea v-model='item.note'
+          placeholder="메모를 입력하세요">
+          </textarea>
+        </div>
+
+      </li>
+    </ul>
+    </h1>
   </div>
-</template>
+</template> 
 
 <script>
+import DayScore from './DayScore.vue'
+
 export default {
   name: 'DayComponent',
-  methods: {},
+  methods: {
+    onUpdateScore(item, score) {
+      item.score = score;
+    }
+  },
   computed: {},
+  components: {
+    'day-score': DayScore
+  },
   data() {
-    return {}
+    return {
+      items : [
+        {
+          action: "",
+          id: '2018-04-01-08-10',
+          note: "",
+          open: false,
+          score: undefined,
+        },
+        {
+          action: "",
+          id: "2018-04-01-10-12",
+          note: "",
+          open: false,
+          score: undefined,
+        },
+        {
+          action: "",
+          id: "2018-04-01-12-14",
+          note: "",
+          open: false,
+          score: undefined,
+        }
+      ]
+    }
     },
   props:{}
 }
@@ -18,18 +85,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
---
+++ src/components/DayScore.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="score">
+  
+    <button @click="updateScore(3)">GOOD</button>
+    <button @click="updateScore(1)">SOSO</button>
+    <button @click="updateScore(0)">BAD</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DayScore',
+  methods: {
+    updateScore(score) {
+      console.log(score);
+      console.log(this.item)
+
+      this.$emit('onUpdateScore',this.item, score); // 부모 컴포넌트에 score wjsekf
+    }
+  },
+  computed: {},
+  data() {
+    return {
+      bottons: [
+        {label: 
+          'GOOD',
+          score: 3
+        },
+        {label: 
+          'SOSO',
+          score: 1
+        },
+        {label: 
+          'BAD',
+          score: 0}
+      ]
+    }
+    },
+  props:[
+    'item'
+  ]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
